b10f9384f584598fb7229ab520ac564016b68ee0
waqasdevpy <sakhiwaqashaider@gmail.com>
2025-10-21 18:12:38 +0500

    pushing first.py

---
+++ first.py
@@ -0,0 +1,14 @@
+# a = 10
+# b = 1.5
+# c = "waqas"
+# d = True
+# e = 10j
+# print("type of a ",type(a))
+# print("type of b ",type(b))
+# print("type of c ",type(c))
+# print("type of d ",type(d))
+# print("type of e ",type(e))
+
+a = " waqas i love you"
+b = "shan"
+print(a* 50)
